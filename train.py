922986ee7a617c2d1aa7fbe2dee816f623519446
dungnguyen9801 <dungnguyen9801@gmail.com>
2019-09-24 13:22:54 -0700

    make test cases

--- train.py
+++ train.py
@@ -1,13 +1,14 @@
+import tensorflow as tf
 def train_one_epoch(model, x, optimizer, loss_func, y=None, batch_size=None, options=None):
     if not batch_size:
         batch_size = len(x)
     y_batch = None
-    loss = None
+    loss = []
     for i in range((len(x) + batch_size - 1)//batch_size):
         x_batch = x[i*batch_size: i*batch_size + batch_size]
         if y is not None:
             y_batch = y[i*batch_size: i*batch_size + batch_size]
         loss_callable = lambda : loss_func(model, x_batch, y_batch, options)
         optimizer.minimize(loss_callable, model.get_trainable_variables())
-        loss = loss_callable()
-    return loss
+        loss.append(loss_callable())
+    return tf.reduce_mean(loss)
