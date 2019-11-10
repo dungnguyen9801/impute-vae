fix hi-vae loss calculation

bayes-vae/model_vae_bayes.py
@@ -46,15 +46,15 @@ class model_vae_bayes():
             sigma_x = tf.math.exp(log_sigma_x)
             mu_x = tf.reshape(mu_x, (-1, *x.shape))
             sigma_x = tf.reshape(sigma_x,(-1, *x.shape))
-            return tf.math.reduce_sum(utils.get_gaussian_densities(x, mu_x, sigma_x)) \
-                + tf.math.reduce_sum(utils.get_gaussian_densities(zs,0,1))
+            return (tf.math.reduce_sum(utils.get_gaussian_densities(x, mu_x, sigma_x)) \
+                + tf.math.reduce_sum(utils.get_gaussian_densities(zs,0,1)))/len(zs)
         return func
 
     def get_func_log_q_z_x(self):
         def func(zs, x):
             mu_z, log_sigma_z = self.encoder(x)
             sigma_z = tf.math.exp(log_sigma_z)
-            return tf.math.reduce_sum(utils.get_gaussian_densities(zs, mu_z, sigma_z))
+            return tf.math.reduce_sum(utils.get_gaussian_densities(zs, mu_z, sigma_z))/len(zs)
         return func
 
     def get_trainable_variables(self):

hi-vae/hi_vae_tests.py
@@ -26,6 +26,13 @@ def hi_vae_random_data_load():
     x = tf.concat([x0,x1,x2,x3,x4,x5], axis=-1).numpy().astype(np.float32)
     return x, column_types
 
+def hi_vae_wine_data_load():
+    column_types=[3] + [-1]*12
+    data = np.loadtxt('../../data/wine.data', delimiter=',').astype(np.float32)
+    classes = tf.one_hot(data[:,0].astype(np.int)-1, 3).numpy()
+    x = tf.concat([classes, data[:, 1:]], axis=-1).numpy().astype(np.float32)
+    return x, column_types
+
 test_cases = \
 {
     'hi_vae1':
@@ -38,8 +45,24 @@ test_cases = \
         'model_class': mhv.model_hi_vae,
         'options':
         {
-            'length': 10,
+            'length': 1,
             'seed': 0
+        },
+        'use_sgd':True,
+        'report_frequency':100
+    },
+    'hi_vae_wine':
+    {
+        'epochs': 2000000,
+        'batch_size': 1,
+        'input_shape': None,
+        'model_dims': (256,2,256),
+        'dataset_loader': hi_vae_wine_data_load,
+        'model_class': mhv.model_hi_vae,
+        'options':
+        {
+            'length': 10,
+            'seed': 1
         },
         'use_sgd':True,
         'report_frequency':100

hi-vae/model_hi_vae.py
@@ -48,6 +48,7 @@ class model_hi_vae():
     def get_func_log_p_xz(self):
         def func(zs, x):
             zs, s_probs, beta, gamma = zs
+            L = tf.shape(zs)[1].numpy()
             s_dim = tf.shape(s_probs)[-1]
             batch = len(s_probs)
             output = self.decoder([
@@ -66,7 +67,7 @@ class model_hi_vae():
                     p = utils.get_gaussian_densities(
                         x[:,i:i+1],
                         tf.reshape(mu_x, [-1, batch, 1]),
-                        tf.reshape(sigma_x, [-1, batch, 1]))
+                        tf.reshape(sigma_x, [-1, batch, 1]))/L
                     p = tf.reshape(p, [s_dim, batch, -1])
                     p = tf.math.reduce_sum(p, axis=-1)
                     p_x_z = p_x_z + p
@@ -78,7 +79,7 @@ class model_hi_vae():
                             tf.broadcast_to(
                                 x[:, i:i+1],
                                 tf.shape(log_lambda_x)),
-                            log_lambda_x)
+                            log_lambda_x)/L
                     p = tf.reshape(p, [s_dim, batch, -1])
                     p = tf.math.reduce_sum(p, axis=-1)
                     p_x_z = p_x_z + p
@@ -86,7 +87,7 @@ class model_hi_vae():
                 else:
                     probs = output[j][0]
                     probs = tf.reshape(probs,[-1, batch, t])
-                    p = tf.math.log(tf.math.reduce_sum(probs * x[:,i:i+t], axis=-1))
+                    p = tf.math.log(tf.math.reduce_sum(probs * x[:,i:i+t], axis=-1))/L
                     p = tf.reshape(p, [s_dim, batch, -1])
                     p = tf.math.reduce_sum(p, axis=-1)
                     p_x_z = p_x_z + p
@@ -97,15 +98,18 @@ class model_hi_vae():
     def get_func_log_q_z_x(self):
         def func(zs, x):
             zs, s_probs, _, _ = zs
+            L = tf.shape(zs)[0].numpy()
+            batch = x.shape[0]
             s_dim = tf.shape(s_probs)[-1]
             zs = tf.transpose(zs, [1,0,2,3])
             _, mu_z, log_sigma_z, _, _ = self.encoder(x)
             sigma_z = tf.math.exp(log_sigma_z)
             densities = tf.transpose(
                 utils.get_gaussian_densities(zs, mu_z, sigma_z),
-                [1,0,2,3])
-            densities = tf.reshape(densities, [s_dim,-1])
-            return tf.matmul(s_probs, densities)
+                [1,0,2,3])/L
+            densities = tf.reshape(densities, [s_dim, batch, -1])
+            densities = tf.math.reduce_sum(densities, axis=-1)
+            return tf.math.reduce_sum(tf.transpose(s_probs) * densities)
         return func
 
     def get_trainable_variables(self):

shared/elbo_calculator.py
@@ -16,7 +16,7 @@ class elbo_calculator():
         zs = model.get_z_generator()(x, options)
         return -(tf.math.reduce_sum(model.get_func_log_p_z()(zs)) 
                 + tf.math.reduce_sum(model.get_func_log_p_x_z()(zs, x))
-                - tf.math.reduce_sum(model.get_func_log_q_z_x()(zs, x)))/len(zs)/len(x)
+                - tf.math.reduce_sum(model.get_func_log_q_z_x()(zs, x)))/model.len(zs)/len(x)
     
     def get_loss_func(self):
         def func(model, x,y=None,options=None):
@@ -30,5 +30,5 @@ class elbo_calculator():
         def func(model, x,y=None,options=None):
             zs = model.get_z_generator()(x, options)
             return -(model.get_func_log_p_xz()(zs, x)
-                - model.get_func_log_q_z_x()(zs, x))/len(zs)/len(x)
+                - model.get_func_log_q_z_x()(zs, x))/len(x)
         return func